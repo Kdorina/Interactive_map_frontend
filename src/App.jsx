75ba760c18b006117d79f75403210b20b6e65e65
Kovács Dorina <kovacsdorina2@gmail.com>
2026-04-28 15:41:38 +0200

    Login/register - AuthPage létrehozása - formázása

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import { useState } from "react"
+import AuthPage from "./components/AuthPage"
 import Map from "./components/Map"
 import Sidebar from "./components/Sidebar"
 import SavedPointsPanel from "./components/SavedPointsPanel"
 import SettingsPanel from "./components/SettingsPanel"
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
   const [points, setPoints] = useState([])
   const [selectedPoint, setSelectedPoint] = useState(null)
   const [activePanel, setActivePanel] = useState(null)
 
-  const [user, setUser] = useState({
-    name: "Kovács Dorina",
-    email: "dorina@example.com",
-    username: "dorina",
-  })
+  const [user, setUser] = useState(null)
+
+  if (!isAuthenticated) {
+    return (
+      <AuthPage
+        onLogin={(loggedInUser) => {
+          setUser(loggedInUser)
+          setIsAuthenticated(true)
+        }}
+      />
+    )
+  }
 
   return (
-    <div style={{ display: "flex", height: "100vh" }}>
+    <div style={{ display: "flex", height: "100vh", position: "relative" }}>
       <Sidebar activePanel={activePanel} setActivePanel={setActivePanel} />
 
       {activePanel === "home" && (
@@ -32,15 +42,18 @@ function App() {
         <SettingsPanel user={user} setUser={setUser} />
       )}
 
-    <div className="profile-button" onClick={() => setActivePanel("settings")}>
-      {user.name.charAt(0).toUpperCase()}
-    </div>
-    
       <Map
         points={points}
         setPoints={setPoints}
         selectedPoint={selectedPoint}
       />
+
+      <div
+        className="profile-button"
+        onClick={() => setActivePanel("settings")}
+      >
+        {user?.name?.charAt(0).toUpperCase()}
+      </div>
     </div>
   )
 }
